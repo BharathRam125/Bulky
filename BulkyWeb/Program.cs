cffaeb08b467f219c4f40ee6a42ba4dc3d0cfd7c
BharathRam125 <bharathram1200@gmail.com>
2023-08-10 21:06:17 +0530

    Model category

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
+ 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
