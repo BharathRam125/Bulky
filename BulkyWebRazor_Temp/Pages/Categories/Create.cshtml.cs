9dc196aa3e7c9a21328ee3038511c67cdda58a5e
BharathRam125 <bharathram1200@gmail.com>
2024-03-03 00:20:27 +0530

    Edit ,delete category pages and controllers created

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+        
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+            
+
+        }
+    }
+}
---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public Category Category { get; set; }
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int? id)
+        {
+            if(id != null && id != 0)
+            {
+                Category = _db.Categories.Find(id);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                _db.SaveChanges();
+                return RedirectToPage("Index");
+            }
+            return Page();
+
+
+        }
+    }
+}
