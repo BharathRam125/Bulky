4c2ea477e170c21793689a08288a02556504df0d
BharathRam125 <bharathram1200@gmail.com>
2024-03-02 14:33:37 +0530

    Category coltroller and ApplicationDbContext Data created

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "SciFi", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 3, Name = "Drama", DisplayOrder = 1 }
+               );
+        }
+    }
+}
